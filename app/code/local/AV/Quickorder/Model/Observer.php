ceb0e4dcb4f12b5d5c7c1b6dee6dfd08153bf060
adamvarga <adam@mage-profis.de>
2018-02-18 17:37:46 +0100

    Clean

--- app/code/local/AV/Quickorder/Model/Observer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AV_Quickorder_Model_Observer {
-
-    public function test(Varien_Event_Observer $observer) {
-
-    }
-
-}
--- app/code/local/AV/Quickorder/Model/Resource/Mysql4/Setup.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class AV_Quickorder_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup {
-
-}
